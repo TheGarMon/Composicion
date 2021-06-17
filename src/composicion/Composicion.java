7004469d1878f6d5f13984ca6de61372b91330b7
Carlos Enrique García Moncada <carlosmoncada56@aragon.unam.mx>
2021-06-17 12:53:47 -0500

    Clase 10/06/2021

---
+++ src/composicion/Composicion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicion;
+
+import ico.fes.objetos.Muro;
+import ico.fes.objetos.Puerta;
+import java.awt.Color;
+
+/**
+ *
+ * @author Carlos GarMon
+ */
+public class Composicion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Muro m1 = new Muro (2.5f, 4.5f, 3.5f, Color.gray, null, 
+                new Puerta("Madera", Color.white, false)  );
+        
+        System.out.println(m1.getAlto() + " mts. de alto");
+        System.out.println(m1.getPuerta().getMaterial());
+        /* ¿Cómo cabiamos el material de la puerta a aluminio?
+        ¿Cómo cambio el color de blanco a azul?
+        ¿Cómo agreegamos una ventana al muro? */
+        
+    }
+    
+}
